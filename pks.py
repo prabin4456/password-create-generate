81a5ec56a3e7a8f64177a8115630842df9161377
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-02 20:00:41 +0545

    Add files via upload

---
+++ pks.py
@@ -0,0 +1,6 @@
+num = int(input("Display multiplication table of? "))
+# use for loop to iterate 100 times
+for i in range(1000):
+	print('password',+i)
+	print('computer',+i)
+	print('wireless',+i)
