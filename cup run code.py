1d7c87741ee3572de66b798a07549ec39dd69960
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-11 09:29:21 +0545

    Update cup run code.py

--- cup run code.py
+++ cup run code.py
@@ -1,43 +1,30 @@
 pkg update
 
-
 pkg update && upgrade
-
 
 pkg install python
 
-
 apt install python
-
 
 pkg install git
 
-
 pkg install python2
 
-
 git clone https://github.com/Mebus/cupp
-
 
 ls
 
 cd cupp
 
-
 python3 cupp.py
-
 
 python3 cupp.py -i
 
-
 ls
-
 
 termux-setup-storage
 
-
 mv name.txt /sdcard
-
 
 cp name.txt /sdcard
 
