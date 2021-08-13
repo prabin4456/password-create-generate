c35802d0b632642efa6cdf96d4c91dd656ed3f53
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-13 15:17:14 +0545

    Add files via upload

---
+++ all coding mix.py
@@ -0,0 +1,25 @@
+#While loop
+i = 0
+while i<=100:
+	print("prabin")
+	i= i+1
+#While loop
+i = 0
+while i<=100:
+	print( i )
+	i+=1
+#days
+days = ['sun','mon','tues']
+for i in days:
+	print(i)
+#123
+for i in range(100):
+	print(i)
+#100
+#100 to 200
+for i in range(100,200,1) :
+	print(i)
+#prabin
+for i in range(5) :
+	for j in range(1):
+		print("prabin")
